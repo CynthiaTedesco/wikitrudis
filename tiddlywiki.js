482baf6c304124c6a3222b0e924458bf13c14ada
Cynthia <cynthia.tedesco@gmail.com>
2017-04-24 16:00:22 -0300

    logging

--- tiddlywiki.js
+++ tiddlywiki.js
@@ -7,7 +7,10 @@ This is invoked as a shell script by NPM when the `tiddlywiki` command is typed
 var $tw = require("./boot/boot.js").TiddlyWiki();
 
 // Pass the command line arguments to the boot kernel
+console.log('-------------------------------------------');
+console.log(process.argv);
 $tw.boot.argv = Array.prototype.slice.call(process.argv,2);
+console.log($tw.boot.argv);
 
 // Boot the TW5 app
 $tw.boot.boot();
